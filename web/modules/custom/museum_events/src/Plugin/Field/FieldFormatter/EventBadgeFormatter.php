1a059428c38eb7059a0e16f7b43aac673922921f
Olena Maziar <olenamaziar@Dmytros-MacBook-Pro.local>
2024-01-19 10:24:43 +0100

    Updated configs

---
+++ web/modules/custom/museum_events/src/Plugin/Field/FieldFormatter/EventBadgeFormatter.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Drupal\museum_events\Plugin\Field\FieldFormatter;
+
+use Drupal\Core\Field\FormatterBase;
+use Drupal\Core\Field\FieldItemListInterface;
+
+/**
+ * Plugin implementation of the 'Event badge' formatter.
+ *
+ * @FieldFormatter(
+ *   id = "field_number_of_tickets_availabl",
+ *   label = @Translation("Badge"),
+ *   field_types = {
+ *     "integer"
+ *   }
+ * )
+ */
+class EventBadgeFormatter extends FormatterBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function settingsSummary() {
+    $summary = [];
+    $summary[] = $this->t('Displays the random string.');
+    return $summary;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function viewElements(FieldItemListInterface $items, $langcode) {
+    $element = [];
+
+    foreach ($items as $delta => $item) {
+      // Render each element as markup.
+      $element[$delta] = ['#markup' => $item->value];
+
+      if($item->value == 0) {
+        $badge = 'Sold Out';
+        $element[$delta] = ['#markup' => $badge];
+      }
+      elseif ($item->value <= 10) {
+        $element[$delta] = ['#markup' => $item->value];
+      }
+    }
+
+    return $element;
+  }
+
+}
